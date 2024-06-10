c410e75e4a1f7b863395d45ef3014e8e9a995da3
Cr-Israel <carlosisrael08@hotmail.com>
2024-06-10 17:49:48 -0300

    feat: migrations

---
+++ db/migrations/20240610201205_create-meals.ts
@@ -0,0 +1,20 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id').primary()
+    table.text('name').notNullable()
+    table.text('description').notNullable()
+    table.timestamp('date_time').notNullable()
+    table.boolean('within_diet').notNullable()
+    table.uuid('user_id').references('id').inTable('users').onDelete('CASCADE')
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals')
+}
+
