5e8cc01b7b0c81ddbcfe13a032b071c6b5f33cbe
Cr-Israel <carlosisrael08@hotmail.com>
2024-06-10 17:51:22 -0300

    ferat: controller get all users register

---
+++ src/infra/controllers/get-users.ts
@@ -0,0 +1,11 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { knex } from "../../database";
+
+export class GetUsers {
+  async handler(request: FastifyRequest, reply: FastifyReply) {
+
+    const users = await knex('users').select()
+
+    return { users }
+  }
+}
