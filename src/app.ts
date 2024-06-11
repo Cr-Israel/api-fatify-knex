7e8d14406954fedc996fb6431a791cefd44e6201
Cr-Israel <carlosisrael08@hotmail.com>
2024-06-11 10:18:29 -0300

    app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import fastify from "fastify"
-import { UserRoutes } from "./infra/routes/user-routes"
+import cookie from "@fastify/cookie"
+
+import { UserRoutes } from "./infrastructure/routes/user-routes"
 
 export const app = fastify()
+
+app.register(cookie)
 
 app.register(UserRoutes)
