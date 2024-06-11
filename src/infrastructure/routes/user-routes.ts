8212355cf4d037ed152dec4e1a9e6cd53693e8cc
Cr-Israel <carlosisrael08@hotmail.com>
2024-06-11 10:19:59 -0300

    feat: prehandler in users routes

---
+++ src/infrastructure/routes/user-routes.ts
@@ -0,0 +1,10 @@
+import { FastifyInstance } from "fastify"
+
+import { CreateUser } from "../controllers/users/create-user"
+import { GetUsers } from "../controllers/users/get-users"
+import { CheckSessionIdExists } from "../middlewares/check-session-id-exists"
+
+export async function UserRoutes(app: FastifyInstance) {
+  app.post('/users', new CreateUser().handler)
+  app.get('/users', { preHandler: [new CheckSessionIdExists().handler] }, new GetUsers().handler)
+}
