3cc8a1a4f351bb285ac2136848040f57f784daac
Cr-Israel <carlosisrael08@hotmail.com>
2024-06-17 15:41:02 -0300

    app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import fastify from "fastify"
 import cookie from "@fastify/cookie"
 
-import { UserRoutes } from "./infrastructure/routes/user-routes"
+import { UserRoutes } from "./infrastructure/routes/users/users-routes"
+import { MealsRoutes } from "./infrastructure/routes/meals/meals-routes"
 
 export const app = fastify()
 
 app.register(cookie)
 
 app.register(UserRoutes)
+app.register(MealsRoutes)
