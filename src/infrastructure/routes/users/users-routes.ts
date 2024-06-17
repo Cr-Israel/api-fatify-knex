96fae45ca73ccbbf7fb8aeaca7164752312153db
Cr-Israel <carlosisrael08@hotmail.com>
2024-06-17 15:41:42 -0300

    user routes

--- src/infrastructure/routes/users/users-routes.ts
+++ src/infrastructure/routes/users/users-routes.ts
@@ -1,8 +1,8 @@
 import { FastifyInstance } from "fastify"
 
-import { CreateUser } from "../controllers/users/create-user"
-import { GetUsers } from "../controllers/users/get-users"
-import { CheckSessionIdExists } from "../middlewares/check-session-id-exists"
+import { CreateUser } from "../../controllers/users/create-user"
+import { GetUsers } from "../../controllers/users/get-users"
+import { CheckSessionIdExists } from "../../middlewares/check-session-id-exists"
 
 export async function UserRoutes(app: FastifyInstance) {
   app.post('/users', new CreateUser().handler)
