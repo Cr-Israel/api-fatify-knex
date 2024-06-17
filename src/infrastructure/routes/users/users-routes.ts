dd80a9bce46aaec5a5e833f067fd3650a8598b49
Cr-Israel <carlosisrael08@hotmail.com>
2024-06-17 17:19:45 -0300

    feat: user routes

--- src/infrastructure/routes/users/users-routes.ts
+++ src/infrastructure/routes/users/users-routes.ts
@@ -1,10 +1,12 @@
 import { FastifyInstance } from "fastify"
 
+import { CheckSessionIdExists } from "../../middlewares/check-session-id-exists"
 import { CreateUser } from "../../controllers/users/create-user"
 import { GetUsers } from "../../controllers/users/get-users"
-import { CheckSessionIdExists } from "../../middlewares/check-session-id-exists"
+import { GetAllUsers } from "../../controllers/users/get-all-users"
 
 export async function UserRoutes(app: FastifyInstance) {
+  // app.get('/users', new GetAllUsers().handler)
   app.post('/users', new CreateUser().handler)
   app.get('/users', { preHandler: [new CheckSessionIdExists().handler] }, new GetUsers().handler)
 }
