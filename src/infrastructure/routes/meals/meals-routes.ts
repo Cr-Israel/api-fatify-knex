f545d4d4f6921dd7d491109be2ec31f281128b53
Cr-Israel <carlosisrael08@hotmail.com>
2024-06-18 15:33:39 -0300

    meals routes

--- src/infrastructure/routes/meals/meals-routes.ts
+++ src/infrastructure/routes/meals/meals-routes.ts
@@ -12,8 +12,8 @@ import { Metrics } from "../../controllers/meals/metrics-meals"
 export async function MealsRoutes(app: FastifyInstance) {
   app.post('/meals', { preHandler: [new CheckSessionIdExists().handler] }, new CreateMeal().handler)
   app.get('/meals', { preHandler: [new CheckSessionIdExists().handler] }, new GetMeals().handler)
-  app.patch('/meals/:mealId', { preHandler: [new CheckSessionIdExists().handler] }, new EditMeal().handler)
-  app.delete('/meals/:mealId', { preHandler: [new CheckSessionIdExists().handler] }, new DeleteMeal().handler)
-  app.get('/meals/:mealId', { preHandler: [new CheckSessionIdExists().handler] }, new GetMealById().handler)
+  app.patch('/meals/:id', { preHandler: [new CheckSessionIdExists().handler] }, new EditMeal().handler)
+  app.delete('/meals/:id', { preHandler: [new CheckSessionIdExists().handler] }, new DeleteMeal().handler)
+  app.get('/meals/:id', { preHandler: [new CheckSessionIdExists().handler] }, new GetMealById().handler)
   app.get('/meals/metrics', { preHandler: [new CheckSessionIdExists().handler] }, new Metrics().handler)
 }
