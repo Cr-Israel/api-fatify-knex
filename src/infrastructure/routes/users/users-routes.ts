98c737570cd0788c7065b61cecbdf45806291bed
Cr-Israel <carlosisrael08@hotmail.com>
2024-06-18 14:17:19 -0300

    feat: add route get all users

--- src/infrastructure/routes/users/users-routes.ts
+++ src/infrastructure/routes/users/users-routes.ts
@@ -6,7 +6,7 @@ import { GetUsers } from "../../controllers/users/get-users"
 import { GetAllUsers } from "../../controllers/users/get-all-users"
 
 export async function UserRoutes(app: FastifyInstance) {
-  // app.get('/users', new GetAllUsers().handler)
+  app.get('/all-users', new GetAllUsers().handler)
   app.post('/users', new CreateUser().handler)
   app.get('/users', { preHandler: [new CheckSessionIdExists().handler] }, new GetUsers().handler)
 }
